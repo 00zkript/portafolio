213a142829eeeff64a0441fc03f1463c3d5eccd3
00zkript <hg305718@gmail.com>
2024-10-08 22:48:26 -0500

    config page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://00zkript.github.io',
+  base: 'portafolio',
   integrations: [
     tailwind(),
     vue()
